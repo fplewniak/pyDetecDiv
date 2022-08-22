6ce2c40db3675b654eecb465dedabbbffb748f60
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2022-08-22 11:57:03 +0200

    Added tables to handle processing results. ImageResource renamed FileResource to accomodate all types. Added mimetype fields wherever useful to define the data type.

--- src/pydetecdiv/database/CreateTables.sql
+++ src/pydetecdiv/database/CreateTables.sql
@@ -1,8 +1,27 @@
+--
+-- File generated with SQLiteStudio v3.3.3 on lun. août 22 11:53:58 2022
+--
+-- Text encoding used: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Dataset
 CREATE TABLE Dataset (
     id   INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT
 );
 
+
+-- Table: FileResource
+CREATE TABLE FileResource (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    locator  TEXT    NOT NULL,
+    mimetype TEXT    NOT NULL
+);
+
+
+-- Table: FOV
 CREATE TABLE FOV (
     id       INTEGER PRIMARY KEY AUTOINCREMENT
                      UNIQUE,
@@ -11,17 +30,21 @@ CREATE TABLE FOV (
     xsize    INTEGER NOT NULL
                      DEFAULT (1000),
     ysize    INTEGER NOT NULL
-                     DEFAULT (1000)
+                     DEFAULT (1000) 
 );
 
+
+-- Table: FOVdata
 CREATE TABLE FOVdata (
     id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    fov       INTEGER REFERENCES FOV (id)
+    fov       INTEGER REFERENCES FOV (id) 
                       NOT NULL,
     imagedata INTEGER REFERENCES ImageData (id) ON DELETE CASCADE
                       NOT NULL
 );
 
+
+-- Table: FOVprocess
 CREATE TABLE FOVprocess (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     fov        INTEGER REFERENCES FOV (id) ON DELETE CASCADE
@@ -30,25 +53,35 @@ CREATE TABLE FOVprocess (
                        NOT NULL
 );
 
+
+-- Table: History
 CREATE TABLE History (
     id   INTEGER  PRIMARY KEY AUTOINCREMENT,
     time DATETIME NOT NULL
 );
 
+
+-- Table: HistoryFOVprocess
 CREATE TABLE HistoryFOVprocess (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     history    INTEGER REFERENCES History (id) ON DELETE CASCADE,
-    fovprocess INTEGER REFERENCES FOVprocess (id) ON DELETE CASCADE
+    fovprocess INTEGER REFERENCES FOVprocess (id) ON DELETE CASCADE,
+    results    INTEGER REFERENCES Results (id) 
 );
 
+
+-- Table: HistoryROIprocess
 CREATE TABLE HistoryROIprocess (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     history    INTEGER REFERENCES History (id) ON DELETE CASCADE
                        NOT NULL,
     roiprocess INTEGER REFERENCES ROIprocess (id) ON DELETE CASCADE
-                       NOT NULL
+                       NOT NULL,
+    results    INTEGER REFERENCES Results (id) 
 );
 
+
+-- Table: Image
 CREATE TABLE Image (
     id     INTEGER PRIMARY KEY AUTOINCREMENT,
     z      INTEGER NOT NULL
@@ -57,18 +90,24 @@ CREATE TABLE Image (
                    DEFAULT (0),
     xdrift INTEGER,
     ydrift INTEGER,
-    data   INTEGER REFERENCES ImageData (id)
+    data   INTEGER REFERENCES ImageData (id) 
 );
 
+
+-- Table: ImageData
 CREATE TABLE ImageData (
     id        INTEGER PRIMARY KEY AUTOINCREMENT,
     name      TEXT    DEFAULT ('original capture'),
     channel   INTEGER NOT NULL
                       DEFAULT (0),
-    xsize     INTEGER NOT NULL
-                      DEFAULT (1000),
-    ysize     INTEGER DEFAULT (1000)
+    x0        INTEGER NOT NULL
+                      DEFAULT (0),
+    y0        INTEGER DEFAULT (0) 
                       NOT NULL,
+    x1        INTEGER NOT NULL
+                      DEFAULT ( -1),
+    y1        INTEGER NOT NULL
+                      DEFAULT ( -1),
     zsize     INTEGER NOT NULL
                       DEFAULT (1),
     tsize     INTEGER NOT NULL
@@ -76,27 +115,29 @@ CREATE TABLE ImageData (
     interval  TIME    DEFAULT (0),
     orderdims TEXT    NOT NULL
                       DEFAULT ('xyzct'),
-    resource  INTEGER REFERENCES ImageResource (id),
-    path      TEXT
+    resource  INTEGER REFERENCES FileResource (id),
+    path      TEXT,
+    mimetype  TEXT
 );
 
+
+-- Table: ImageDataset
 CREATE TABLE ImageDataset (
     id      INTEGER PRIMARY KEY AUTOINCREMENT,
     dataset INTEGER REFERENCES Dataset (id) ON DELETE CASCADE,
     image   INTEGER REFERENCES Image (id) ON DELETE CASCADE
 );
 
-CREATE TABLE ImageResource (
-    id      INTEGER PRIMARY KEY AUTOINCREMENT,
-    locator TEXT    NOT NULL
-);
 
+-- Table: Processing
 CREATE TABLE Processing (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     processor  INTEGER REFERENCES Processor (id),
     parameters TEXT
 );
 
+
+-- Table: Processor
 CREATE TABLE Processor (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     name        TEXT    NOT NULL,
@@ -104,6 +145,34 @@ CREATE TABLE Processor (
     description TEXT
 );
 
+
+-- Table: ResultData
+CREATE TABLE ResultData (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    results  INTEGER REFERENCES Results (id),
+    resource INTEGER REFERENCES FileResource (id),
+    path     TEXT,
+    mimetype TEXT    NOT NULL
+);
+
+
+-- Table: ResultImageData
+CREATE TABLE ResultImageData (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    results   INTEGER REFERENCES Results (id),
+    imagedata INTEGER REFERENCES ImageData (id) 
+);
+
+
+-- Table: Results
+CREATE TABLE Results (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    classification TEXT,
+    freetext       TEXT
+);
+
+
+-- Table: ROI
 CREATE TABLE ROI (
     id   INTEGER PRIMARY KEY AUTOINCREMENT
                  UNIQUE,
@@ -112,9 +181,11 @@ CREATE TABLE ROI (
     x0   INTEGER DEFAULT (0),
     y0   INTEGER DEFAULT (0),
     x1   INTEGER DEFAULT ( -1),
-    y1   INTEGER DEFAULT ( -1)
+    y1   INTEGER DEFAULT ( -1) 
 );
 
+
+-- Table: ROIdata
 CREATE TABLE ROIdata (
     id        INTEGER PRIMARY KEY AUTOINCREMENT,
     roi       INTEGER REFERENCES ROI (id) ON DELETE CASCADE
@@ -123,6 +194,8 @@ CREATE TABLE ROIdata (
                       REFERENCES ImageData (id) ON DELETE CASCADE
 );
 
+
+-- Table: ROIprocess
 CREATE TABLE ROIprocess (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     roi        INTEGER REFERENCES ROI (id) ON DELETE CASCADE
@@ -130,5 +203,7 @@ CREATE TABLE ROIprocess (
     processing INTEGER REFERENCES Processing (id) ON DELETE CASCADE
                        NOT NULL
 );
-COMMIT;
 
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
