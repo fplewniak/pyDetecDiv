f391a406281febad02445f5a453938dd1f64bfa8
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2022-09-09 16:46:05 +0200

    Created a DAO parent class defining the common behaviour of all DAO subclasses to reduce code redundancy.

--- src/pydetecdiv/persistence/sqlalchemy/dao/orm.py
+++ src/pydetecdiv/persistence/sqlalchemy/dao/orm.py
@@ -15,45 +15,64 @@ Base = mapper_registry.generate_base()
 tables = Tables()
 
 
-class FOVdao(Base):
+class DAO:
     """
-    DAO class for access to FOV records from the SQL database
+    Data Access Object class defining methods common to all DAOs. Actual DAOs should inherit of this class first in
+    order to inherit the __init__ method.
     """
-    __table__ = tables.list['FOV']
+    __table__ = None
 
     def __init__(self, engine):
         self.engine = engine
 
     def get_records(self, where_clause):
         """
-        A method to get from the SQL database, all FOV records verifying the specified where clause
+        A method to get from the SQL database, all records verifying the specified where clause
         :param where_clause: the selection 'where clause' that can be specified using DAO classes or tables
         :type where_clause: a sqlachemy where clause defining the SQL selection query
-        :return: a list of FOV records as dictionaries
+        :return: a list of records as dictionaries
         :rtype: list of dict
         """
         stmt = self.__table__.select(where_clause)
         result = self.engine.execute(stmt)
-        return [FOVdao.create_record(fov) for fov in DataFrame(result.mappings()).to_dict('records')]
+        return [self.__class__.create_record(rec) for rec in DataFrame(result.mappings()).to_dict('records')]
 
     @staticmethod
-    def create_record(fov):
+    def create_record(rec):
         """
-        A private method creating a record dictionary from a fov row dictionary. This method is used to convert the SQL
+        Template for method converting a DAO row dictionary into a DSO record
+        :param rec: the DAO record to convert into a DSO record
+        :type rec: dict
+        :return: a DSO record
+        :rtype: dict
+        """
+        raise NotImplementedError('Call to a create_record(rec) method that is not implemented')
+
+
+class FOVdao(DAO, Base):
+    """
+    DAO class for access to FOV records from the SQL database
+    """
+    __table__ = tables.list['FOV']
+
+    @staticmethod
+    def create_record(rec):
+        """
+        A method creating a DAO record dictionary from a fov row dictionary. This method is used to convert the SQL
         table columns into the FOV record fields expected by the domain layer
-        :param fov: The dictionary created by the SQL database query and representing the data as it is stored in the
+        :param rec: The dictionary created by the SQL database query and representing the data as it is stored in the
         database. This dictionary will be converted by this method into a dictionary record suitable for the domain
         layer
-        :type fov: dict
+        :type rec: dict
         :return a FOV record as a dictionary with keys() appropriate for handling by the domain layer
         :rtype: dict
         """
-        return {'id': fov['id'],
-                'name': fov['name'],
-                'comments': fov['comments'],
+        return {'id': rec['id'],
+                'name': rec['name'],
+                'comments': rec['comments'],
                 'top_left': (0, 0),
-                'bottom_right': (fov['xsize'] - 1, fov['ysize'] - 1),
-                'size': (fov['xsize'], fov['ysize']),
+                'bottom_right': (rec['xsize'] - 1, rec['ysize'] - 1),
+                'size': (rec['xsize'], rec['ysize']),
                 }
 
     def roi_list(self, fov_id):
@@ -68,43 +87,28 @@ class FOVdao(Base):
         return roi_dao.get_records(tables.list['ROI'].c.fov == fov_id)
 
 
-class ROIdao(Base):
+class ROIdao(DAO, Base):
     """
     DAO class for access to ROI records from the SQL database
     """
     __table__ = tables.list['ROI']
 
-    def __init__(self, engine):
-        self.engine = engine
-
-    def get_records(self, where_clause):
-        """
-        A method to get from the SQL database, all ROI records verifying the specified where clause
-        :param where_clause: the selection 'where clause' that can be specified using DAO classes or tables
-        :type where_clause: a sqlachemy where clause defining the SQL selection query
-        :return: a list of ROI records as dictionaries
-        :rtype: list of dict
-        """
-        stmt = self.__table__.select(where_clause)
-        result = self.engine.execute(stmt)
-        return [ROIdao.create_record(roi) for roi in DataFrame(result.mappings()).to_dict('records')]
-
     @staticmethod
-    def create_record(roi):
+    def create_record(rec):
         """
-        A private method creating a record dictionary from a roi row dictionary. This method is used to convert the SQL
+        A method creating a record dictionary from a roi row dictionary. This method is used to convert the SQL
         table columns into the ROI record fields expected by the domain layer
-        :param roi: The dictionary created by the SQL database query and representing the data as it is stored in the
+        :param rec: The dictionary created by the SQL database query and representing the data as it is stored in the
         database. This dictionary will be converted by this method into a dictionary record suitable for the domain
         layer
-        :type roi: dict
+        :type rec: dict
         :return a ROI record as a dictionary with keys() appropriate for handling by the domain layer
         :rtype: dict
         """
-        return {'id': roi['id'],
-                'name': roi['name'],
-                'fov': roi['fov'],
-                'top_left': (roi['x0'], roi['y0']),
-                'bottom_right': (roi['x1'], roi['y1']),
-                'size': (roi['x1'] - roi['x0'] + 1, roi['y1'] - roi['y0'] + 1)
+        return {'id': rec['id'],
+                'name': rec['name'],
+                'fov': rec['fov'],
+                'top_left': (rec['x0'], rec['y0']),
+                'bottom_right': (rec['x1'], rec['y1']),
+                'size': (rec['x1'] - rec['x0'] + 1, rec['y1'] - rec['y0'] + 1)
                 }
