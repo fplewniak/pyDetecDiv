3e356a7682c3f77c0093e9f7d1ec239f26078bee
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2022-09-22 14:37:32 +0200

    Linker.association(class1_name, class2_name) static method directly returns the appropriate association class instead of a dictionary.

--- src/pydetecdiv/persistence/sqlalchemy/orm/associations.py
+++ src/pydetecdiv/persistence/sqlalchemy/orm/associations.py
@@ -9,20 +9,33 @@ from pydetecdiv.persistence.sqlalchemy.orm.main import Base
 
 
 class Linker:
+    """
+    A class providing methods for linking two Data access objects.
+    """
     @staticmethod
-    def associations():
-        return {
+    def association(class1_name, class2_name):
+        """
+        Defines the possible associations between DAO classes and returns the one corresponding to the classes passed
+         as arguments.
+        :return:
+        """
+        association_list = {
             ('FOVdao', 'ImageDataDao'): FovData,
             ('ImageDataDao', 'FOVdao'): FovData,
         }
+        return association_list[(class1_name, class2_name)]
 
     @staticmethod
     def link(obj1, obj2):
-        return Linker.associations()[(obj1.__class__.__name__, obj2.__class__.__name__)].link(obj1, obj2)
-
-    @staticmethod
-    def unlink(obj1, obj2):
-        ...
+        """
+        Creates a link between the specified objects, calling the appropriate association object link method, as
+        provided by the associations() method above.
+        :param obj1: the first object to link
+        :type obj1: DAO
+        :param obj2: the second object to link
+        :type obj2: DAO
+        """
+        Linker.association(obj1.__class__.__name__, obj2.__class__.__name__).link(obj1, obj2)
 
 
 class FovData(Base):
@@ -37,14 +50,31 @@ class FovData(Base):
 
     @staticmethod
     def fov_to_image_data():
+        """
+        Defines and returns the relationship from FOV to ImageData tables
+        :return: the relationship between FOV and ImageData
+        :rtype: sqlalchemy.orm.RelationshipProperty object
+        """
         return relationship("FovData", back_populates='fov_', lazy='joined')
 
     @staticmethod
     def image_data_to_fov():
+        """
+        Defines and returns the relationship from ImageData to FOV tables
+        :return: the relationship between ImageData and FOV
+        :rtype: sqlalchemy.orm.RelationshipProperty object
+        """
         return relationship("FovData", back_populates='image_data_', lazy='joined')
 
     @staticmethod
     def link(obj1, obj2):
+        """
+        Creates a link between the specified FOVdao and ImageDataDao objects.
+        :param obj1: the first object to link
+        :type obj1: FOVdao or ImageDataDao
+        :param obj2: the second object to link
+        :type obj2: ImageDataDao or FOVdao
+        """
         a = FovData()
         if obj1.__class__.__name__ == 'FOVdao':
             a.image_data_ = obj2
