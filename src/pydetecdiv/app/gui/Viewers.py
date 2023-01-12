c818173ed484e51455a76660bcf9cedd8425a019
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2023-01-12 16:07:15 +0100

    Added docstrings

--- src/pydetecdiv/app/gui/Viewers.py
+++ src/pydetecdiv/app/gui/Viewers.py
@@ -1,14 +1,26 @@
 #  CeCILL FREE SOFTWARE LICENSE AGREEMENT Version 2.1 dated 2013-06-21
 #  Frédéric PLEWNIAK, CNRS/Université de Strasbourg UMR7156 - GMGM
+"""
+Classes to handle data viewers for visualization of images, tables, plots, etc
+"""
 import dearpygui.dearpygui as dpg
 
 
-class ImageViewer():
+class ImageViewer:
+    """
+    Image viewer to visualize image data
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.active = False
+        self.image_resource = None
 
     def clear(self):
+        """
+        Clear the image viewer if it was in use
+        :return: the Image viewer object
+        :rtype: ImageViewer
+        """
         if self.active:
             dpg.delete_item('image_registry')
             dpg.delete_item('image_viewer')
@@ -18,8 +30,21 @@ class ImageViewer():
         return self
 
     def imshow(self, image_resource, c=0, z=0, t=0):
+        """
+        Show an image contained in image resource for the specified channel, layer and frame
+        :param image_resource: the image resource containing the image data
+        :type image_resource: SingleTiff or MultipleTiff object
+        :param c: the channel index
+        :type c: int
+        :param z: the layer index
+        :type z: int
+        :param t: the frame index
+        :type t: int
+        :return: the ImageViewer object
+        :rtype: ImageViewer
+        """
         self.image_resource = image_resource
-        width, height, channels, data = image_resource.as_texture(c=c, z=z, t=t)
+        width, height, _, data = image_resource.as_texture(c=c, z=z, t=t)
         with dpg.texture_registry(show=False, tag='image_registry'):
             dpg.add_dynamic_texture(width=width, height=height, default_value=data, tag="image_to_view")
         with dpg.group(parent='viewer_window', tag='image_control'):
@@ -43,14 +68,38 @@ class ImageViewer():
         self.active = True
         return self
 
-    def change_time(self, sender, app_data, user_data):
-        width, height, channels, data = self.image_resource.as_texture(c=user_data['c'], t=app_data, z=user_data['z'], )
+    def change_time(self, _, app_data, user_data):
+        """
+        Callback method to change display to another frame
+        :param _: dummy sender parameter that is not used
+        :param app_data: the frame index
+        :type app_data: int
+        :param user_data: channel and layer indices
+        :type user_data: dict
+        """
+        _, _, _, data = self.image_resource.as_texture(c=user_data['c'], t=app_data, z=user_data['z'], )
         dpg.set_value("image_to_view", data)
 
-    def change_layer(self, sender, app_data, user_data):
-        width, height, channels, data = self.image_resource.as_texture(c=user_data['c'], t=user_data['t'], z=app_data, )
+    def change_layer(self, _, app_data, user_data):
+        """
+        Callback method to change display to another layer
+        :param _: dummy sender parameter that is not used
+        :param app_data: the layer index
+        :type app_data: int
+        :param user_data: channel and frame indices
+        :type user_data: dict
+        """
+        _, _, _, data = self.image_resource.as_texture(c=user_data['c'], t=user_data['t'], z=app_data, )
         dpg.set_value("image_to_view", data)
 
-    def change_channel(self, sender, app_data, user_data):
-        width, height, channels, data = self.image_resource.as_texture(c=app_data, t=user_data['t'], z=user_data['z'], )
+    def change_channel(self, _, app_data, user_data):
+        """
+        Callback method to change display to another channel
+        :param _: dummy sender parameter that is not used
+        :param app_data: the channel index
+        :type app_data: int
+        :param user_data: frame and layer indices
+        :type user_data: dict
+        """
+        _, _, _, data = self.image_resource.as_texture(c=app_data, t=user_data['t'], z=user_data['z'], )
         dpg.set_value("image_to_view", data)
