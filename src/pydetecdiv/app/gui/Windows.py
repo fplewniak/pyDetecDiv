98ec935d11c5f0be1c80766b9cfc95bc98349709
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2023-02-02 12:37:45 +0100

    Added Open project feature, setting a global variable pydetecdiv.app.project pointing to the open project. For a more convenient access to the main window from anywhere in the application, a global pydetecdiv.app.main_window variable has also been defined that is initiated to self in the __init__ method of the MainWindow class.

--- src/pydetecdiv/app/gui/Windows.py
+++ src/pydetecdiv/app/gui/Windows.py
@@ -1,12 +1,13 @@
 from PySide6.QtGui import QCloseEvent
 from PySide6.QtWidgets import QMainWindow
 from pydetecdiv.app.gui import MainToolBar, MainStatusBar, FileMenu
-from pydetecdiv.app import get_settings
+import pydetecdiv.app
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        pydetecdiv.app.main_window = self
         self.setObjectName('PyDetecDiv main window')
 
         self.addToolBar(MainToolBar(self, 'main toolbar'))
@@ -15,11 +16,11 @@ class MainWindow(QMainWindow):
 
         self.setStatusBar(MainStatusBar())
 
-        settings = get_settings()
+        settings = pydetecdiv.app.get_settings()
         self.restoreGeometry(settings.value("geometry"))
         self.restoreState(settings.value("windowState"))
 
     def closeEvent(self, event: QCloseEvent) -> None:
-        settings = get_settings()
+        settings = pydetecdiv.app.get_settings()
         settings.setValue("geometry", self.saveGeometry())
         settings.setValue("windowState", self.saveState())
