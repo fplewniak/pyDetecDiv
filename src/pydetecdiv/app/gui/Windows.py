f625fa8e75a805a45163ae363ca12686f8e504fe
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2023-04-18 11:21:29 +0200

    Removed reference to ResourceMenu class which will not be used

--- src/pydetecdiv/app/gui/Windows.py
+++ src/pydetecdiv/app/gui/Windows.py
@@ -5,7 +5,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QCursor
 from PySide6.QtWidgets import QMainWindow, QMdiArea, QTabWidget, QDockWidget, QFormLayout, QLabel, QComboBox, \
     QDialogButtonBox, QWidget, QFrame, QMenuBar
-from pydetecdiv.app.gui import MainToolBar, MainStatusBar, FileMenu, DataMenu, ResourceMenu
+from pydetecdiv.app.gui import MainToolBar, MainStatusBar, FileMenu, DataMenu
 from pydetecdiv.app import get_settings, PyDetecDiv, pydetecdiv_project
 
 from pydetecdiv.app.gui.ImageViewer import ImageViewer
@@ -72,7 +72,6 @@ class TabbedViewer(QTabWidget):
         super().__init__()
         self.viewer = ImageViewer()
         self.viewer.setMenuBar(QMenuBar())
-        ResourceMenu(self.viewer)
         self.setWindowTitle(title)
         self.setDocumentMode(True)
         self.addTab(self.viewer, 'Image viewer')
