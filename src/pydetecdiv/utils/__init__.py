41d13edc58ecdb9b2af169b51a3697f76fee81a1
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2023-04-28 17:29:23 +0200

    Function round_to_even() to round a float to and even integer

--- src/pydetecdiv/utils/__init__.py
+++ src/pydetecdiv/utils/__init__.py
@@ -1,10 +1,24 @@
 #  CeCILL FREE SOFTWARE LICENSE AGREEMENT Version 2.1 dated 2013-06-21
 #  Frédéric PLEWNIAK, CNRS/Université de Strasbourg UMR7156 - GMGM
+import numpy as np
+
 
 def singleton(class_):
     instances = {}
+
     def getinstance(*args, **kwargs):
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
+
     return getinstance
+
+
+def round_to_even(value, ceil=True):
+    rounded = int(np.around(value))
+    if rounded % 2 != 0:
+        if ceil:
+            rounded += 1
+        else:
+            rounded -= 1
+    return rounded
