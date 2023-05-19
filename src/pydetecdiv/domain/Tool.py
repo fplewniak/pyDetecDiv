3813df7ecd0bdece8776b6f69008add5b2d921d0
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2023-05-19 14:36:42 +0200

    Added docstrings

--- src/pydetecdiv/domain/Tool.py
+++ src/pydetecdiv/domain/Tool.py
@@ -1,11 +1,9 @@
 """
-
+Tool module to handle tool definition, requirements and running them in an appropriate environment.
 """
 import os
 import re
 import xml
-
-import numpy as np
 import yaml
 
 
@@ -37,13 +35,29 @@ class Requirements:
         return self.element.attrib
 
     def check_env(self, env):
+        """
+        Check the environment for this tool already exists
+        :param env: the environment to check for existence
+        :type env: dict
+        :return: True if environment exists, False otherwise
+        :rtype: bool
+        """
         match env['type']:
             case 'conda':
-                print(f'conda env list --json')
+                print('conda env list --json')
                 return False
         return True
 
     def check_packages(self, env, packages):
+        """
+        Check the required packages are already installed in the environment
+        :param env: the environment
+        :type env: dict
+        :param packages: the packages to check installation for
+        :type packages: list of str
+        :return: True if all packages are installed, False otherwise
+        :rtype: bool
+        """
         match env['type']:
             case 'conda':
                 print(f'conda list -n {env["env"]}')
@@ -51,6 +65,9 @@ class Requirements:
         return True
 
     def install(self):
+        """
+        Install the requirements if needed
+        """
         packages = ' '.join(self.packages)
         if not self.check_env(self.environment):
             self.install_env(self.environment)
@@ -58,11 +75,23 @@ class Requirements:
             self.install_packages(self.environment, packages)
 
     def install_env(self, env):
+        """
+        Install the environment
+        :param env: the environment
+        :type env: dict
+        """
         match env['type']:
             case 'conda':
                 print(f'conda create --name {env["env"]}')
 
     def install_packages(self, env, packages):
+        """
+        Install packages in environment
+        :param env: the environment
+        :type env: dict
+        :param packages: the packages to install
+        :type packages: list of str
+        """
         match env['type']:
             case 'conda':
                 print(f'conda activate {env["env"]}')
@@ -70,55 +99,110 @@ class Requirements:
 
 
 class Inputs:
+    """
+    A class to handle Tool's input as defined in the configuration file
+    """
+
     def __init__(self, element):
         self.element = element
 
     @property
     def parameters(self):
+        """
+        All the parameters that should be passed to the command line
+        :return: list of parameters
+        :rtype: list
+        """
         return [p.attrib for p in self.element.findall('param')]
 
 
 class Tool:
+    """
+    A class for handling and running tools specified by XML files
+    """
+
     def __init__(self, path):
         self.path = os.path.dirname(path)
         self.xml_tree = xml.etree.ElementTree.parse(path)
         shed_file = os.path.join(os.path.dirname(path), '.shed.yml')
-        with open(shed_file) as file:
+        with open(shed_file, encoding='utf-8') as file:
             self.shed_content = yaml.load(file, Loader=yaml.FullLoader)
 
     @property
     def root(self):
+        """
+        Convenience property returning the root of the Tool's XML configuration
+        :return: the root of the XML tool definition
+        :rtype: xml.etree.Element 'tool'
+        """
         return self.xml_tree.getroot()
 
     @property
     def name(self):
+        """
+        Property returning the name of the tool
+        :return: tool name
+        :rtype: str
+        """
         return self.root.get('name')
 
     @property
     def version(self):
+        """
+        Property returning the version of the tool
+        :return: tool version
+        :rtype: str
+        """
         return self.root.get('version')
 
     @property
     def categories(self):
+        """
+        Property returning the categories the tool belongs to
+        :return: tool categories
+        :rtype: list of str
+        """
         return self.shed_content["categories"]
 
     @property
     def requirements(self):
+        """
+        Property returning the requirements for this tool
+        :return: the environment and packages requirement to run the tool
+        :rtype: Requirements object
+        """
         return Requirements(self.root.find('./requirements/package'))
 
     @property
     def command(self):
+        """
+        Property returning the command line
+        :return:
+        """
         return re.sub(r'\$__tool_directory__', os.path.join(self.path, ''), self.root.find('command').text)
 
     @property
     def attributes(self):
+        """
+        Convenience property returning the tool's attributes as defined in the root node
+        :return: attributes
+        :rtype: dict
+        """
         return self.root.attrib
 
     @property
     def inputs(self):
+        """
+        Property providing the defined inputs for the tool
+        :return: input parameters to run the tool
+        :rtype: Inputs object
+        """
         return Inputs(self.root.find('./inputs'))
 
     def run(self):
+        """
+        Install the requirements if needed and run the tool
+        """
         self.requirements.install()
         for command in str.splitlines(self.command):
             print(command.strip())
