fixed wrong order of X, Y dimensions when returning empty image data

src/pydetecdiv/domain/MultiFileImageResource.py
@@ -114,7 +114,7 @@ class MultiFileImageResource(ImageResourceData):
                                            [0, 1, -drift.dy]]),
                                       (data.shape[1], data.shape[0]))
             return data
-        return np.zeros((self.sizeX, self.sizeY), np.float16)
+        return np.zeros((self.sizeY, self.sizeX), np.float16)
         # return None
 
     def data_sample(self, X=None, Y=None):