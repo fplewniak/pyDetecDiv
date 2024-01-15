69297202d265cdab0084af7a4e88da724b4f26c7
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2024-01-15 10:26:35 +0100

    Proof of concept for a class annotator tab for ROI classification plugin. Now the class name changes from frame to frame and is replaced (not overwritten) when annotation is set. All classes are memorized and displayed properly when changing frame.

--- src/pydetecdiv/plugins/roi_classification/gui/annotate.py
+++ src/pydetecdiv/plugins/roi_classification/gui/annotate.py
@@ -18,6 +18,7 @@ def open_annotator_tab(selected_roi, scene):
     project_window.parent().parent().addTab(viewer, viewer.image_source_ref.data(0))
     # viewer.set_image_resource_data(ArrayImageResource(data=data, fov=self.image_resource_data.fov, image_resource=self.image_resource_data.image_resource), crop=crop)
     viewer.set_image_resource_data(project_window.image_resource_data, crop=crop)
+    viewer.roi_classes = ['-'] * viewer.image_resource_data.sizeT
     viewer.ui.view_name.setText(f'View: {viewer.image_source_ref.data(0)}')
     viewer.synchronize_with(project_window)
     viewer.display()
@@ -34,16 +35,27 @@ class Annotator(ImageViewer):
         self.scene.setParent(self)
         self.viewport_rect = None
         self.zoom_set_value(200)
+        self.class_item = QGraphicsTextItem('-')
+        self.roi_classes = []
+        self.class_names = ['one', 'two', 'three', 'four']
+        self.scene.addItem(self.class_item)
 
-    def display(self, C=None, T=None, Z=None, class_name=' - '):
+    def annotate_current_roi(self, class_name='-'):
+        self.roi_classes[self.T] = class_name
+        self.display()
+
+    def display(self, C=None, T=None, Z=None):
         super().display(C, T, Z)
-        class_item = QGraphicsTextItem(class_name)
-        text_boundingRect = class_item.boundingRect()
+        self.display_class_name()
+
+    def display_class_name(self):
+        self.class_item.setPlainText(self.roi_classes[self.T])
+        text_boundingRect = self.class_item.boundingRect()
         frame_boundingRect = self.pixmapItem.boundingRect()
-        class_item.setPos((frame_boundingRect.width() - text_boundingRect.width())/2 , frame_boundingRect.height())
-        self.scene.addItem(class_item)
-        self.viewport_rect = QRectF(min([class_item.x(), self.pixmapItem.x()]),
-                                    min([class_item.y(), self.pixmapItem.y()]) - 5,
+        self.class_item.setPos((frame_boundingRect.width() - text_boundingRect.width()) / 2,
+                               frame_boundingRect.height())
+        self.viewport_rect = QRectF(min([self.class_item.x(), self.pixmapItem.x()]),
+                                    min([self.class_item.y(), self.pixmapItem.y()]) - 5,
                                     max([text_boundingRect.width(), frame_boundingRect.width()]),
                                     text_boundingRect.height() + frame_boundingRect.height() + 5,
                                     )
@@ -68,10 +80,9 @@ class AnnotatorScene(ViewerScene):
         super().__init__()
 
     def keyPressEvent(self, event):
-        print(event)
-        if event.text() == '1':
-            self.parent().display(class_name='class 1')
+        if event.text() in ['1', '2', '3', '4']:
+            self.parent().annotate_current_roi(class_name=f'{self.parent().class_names[int(event.text()) - 1]}')
         elif event.matches(QKeySequence.MoveToNextChar):
-            self.parent().change_frame(self.parent().T + 1)
+            self.parent().change_frame(min(self.parent().T + 1, self.parent().image_resource_data.sizeT - 1))
         elif event.matches(QKeySequence.MoveToPreviousChar):
-            self.parent().change_frame(self.parent().T - 1)
+            self.parent().change_frame(max(self.parent().T - 1, 0))
