5d2da21b38033f92f9357f0bf665e47f6fcd82b1
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2024-03-28 10:20:15 +0100

    Added new model combining Resnet50V2 with a LSTM, reproducing the architecture of the DetecDiv div1 model

---
+++ src/pydetecdiv/plugins/roi_classification/models/ResNet50V2_lstm/__init__.py
@@ -0,0 +1,7 @@
+#    This file was created by
+#    MATLAB Deep Learning Toolbox Converter for TensorFlow Models.
+#    03-Nov-2023 11:54:04
+
+from . import model
+
+class_names = ['clog', 'dead', 'empty', 'large', 'small', 'unbud']
