fcd2dac66fd14a299e4eb4c785e8436e98d0338e
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2024-04-05 10:45:48 +0200

    Removed models exported from MATLAB which do not work anyway...

--- src/pydetecdiv/plugins/roi_classification/models/netCNN_div1_10/__init__.py
+++
@@ -1,7 +0,0 @@
-#    This file was created by
-#    MATLAB Deep Learning Toolbox Converter for TensorFlow Models.
-#    18-Oct-2023 15:06:04
-
-from . import model
-
-class_names = ['clog', 'dead', 'empty', 'large', 'small', 'unbud']
