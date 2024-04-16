afbbbfa5f9aae5a77bd9f07f66c62be419dbe508
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2024-04-16 13:46:28 +0200

    ROI classification plugin: implemented InceptionV3 + LSTM, EInceptionResNetV2 + LSTM and Xception + LSTM

---
+++ src/pydetecdiv/plugins/roi_classification/models/InceptionV3_lstm/model.py
@@ -0,0 +1,55 @@
+#    This file was created by
+#    MATLAB Deep Learning Toolbox Converter for TensorFlow Models.
+#    03-Nov-2023 11:54:04
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def create_model():
+    resnet = keras.applications.InceptionV3(input_shape=(75,75,3), weights='imagenet', include_top=False)
+
+    input = keras.Input(shape=(None,75,75,3))
+    fold_out, fold_miniBatchSize = SequenceFoldingLayer((75,75,3))(input)
+
+    central_block = resnet(fold_out, training=True)
+    plugout = keras.layers.GlobalAveragePooling2D()(central_block)
+
+    # unfolding layer should be the same size as the output from the previous block
+    unfold = SequenceUnfoldingLayer((1,1,2048))(plugout, fold_miniBatchSize)
+    unfoldperm = layers.TimeDistributed(layers.Permute((3,2,1)))(unfold)
+    flatten = layers.TimeDistributed(layers.Flatten())(unfoldperm)
+    flatten_bilstm_input = flatten
+    bilstm = layers.Bidirectional(layers.LSTM(150, activation='tanh', recurrent_activation='sigmoid',
+                                              return_sequences=True, return_state=False),
+                                  name="bilstm_")(flatten_bilstm_input)
+    drop = layers.Dropout(0.500000)(bilstm)
+    fc = layers.Dense(6, name="fc_")(drop)
+    softmax = layers.Softmax()(fc)
+    classification = softmax
+
+    model = keras.Model(inputs=[input], outputs=[classification])
+    return model
+
+## Helper layers:
+
+class SequenceFoldingLayer(tf.keras.layers.Layer):
+    def __init__(self, dataShape, name=None):
+        super(SequenceFoldingLayer, self).__init__(name=name)
+        # print(dataShape)
+        self.dataShape = dataShape;
+
+    def call(self, input):
+        # Two outputs: Y and batchSize
+        shape = tf.shape(input)
+        return tf.reshape(input, (-1,) + self.dataShape), shape[0]
+
+
+class SequenceUnfoldingLayer(tf.keras.layers.Layer):
+    def __init__(self, dataShape, name=None):
+        super(SequenceUnfoldingLayer, self).__init__(name=name)
+        self.dataShape = dataShape;
+
+    def call(self, X, batchSize):
+        # print(X.shape)
+        return tf.reshape(X, (batchSize, -1) + self.dataShape)
