36f02fb83887c26b23d7cfd09fa3c7c2c6971acc
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2024-08-07 14:39:43 +0200

    Parameters values that are a list or a dictionary are automatically converted to json and stored as such in the Parameter value. When requesting the value, the json string is converted back and the corresponding list or dictionary is returned. This should ensure consistency of treatment whenever there is a need for conversion between list or dict and a string (choosing the list in ComboBox, saving in database as JSON, etc.)

--- src/pydetecdiv/plugins/parameters/__init__.py
+++ src/pydetecdiv/plugins/parameters/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from PySide6.QtCore import QObject, Signal
 
 
@@ -25,10 +27,15 @@ class Parameter(QObject):
 
     @property
     def value(self):
-        return self._value
+        try:
+            return json.loads(self._value)
+        except:
+            return self._value
 
     def set_value(self, value):
         if self.validate(value):
+            if isinstance(value, (list, dict)):
+                value = json.dumps(value)
             self._value = value
             self.changed.emit(value)
 
@@ -88,8 +95,6 @@ class Parameters:
     """
 
     def __init__(self, parameters=None):
-        # self.plugin = plugin
-        # self.param_groups = {'default': {}} if parameters is None else parameters
         if isinstance(parameters, list):
             self.parameter_list = parameters
         else:
@@ -117,68 +122,3 @@ class Parameters:
 
     def to_dict(self):
         return {param.name: param for param in self.parameter_list}
-
-    # def add_groups(self, groups):
-    #     """
-    #     Add empty groups of parameters
-    #
-    #     :param groups: the list of groups
-    #     """
-    #     groups = groups if isinstance(groups, list) else [groups]
-    #     for group in groups:
-    #         self._add_group(group)
-    #
-    # def _add_group(self, group, param_dict=None):
-    #     """
-    #     Add a new group of parameters (in a dictionary)
-    #
-    #     :param group: the group to create
-    #     :param param_dict: the dictionary of parameters
-    #     """
-    #     self.param_groups[group] = param_dict if param_dict is not None else {}
-    #
-    # def add(self, params, group='default'):
-    #     """
-    #     Add parameters (in a dictionary) to an existing group of parameters
-    #
-    #     :param group: the group of parameters to expand
-    #     :param params: the parameters
-    #     """
-    #     params = params if isinstance(params, list) else [params]
-    #
-    #     if group in self.param_groups:
-    #         self.param_groups[group].update({p.name: p for p in params})
-    #     else:
-    #         self._add_group(group, {p.name: p for p in params})
-    #
-    # def reset(self):
-    #     for group in self.param_groups.keys():
-    #         for param in self.param_groups[group].values():
-    #             param.reset()
-    #
-    # def get_values(self, groups=None):
-    #     """
-    #     Get a dictionary containing all parameters key/values for a given group
-    #
-    #     :param group: the requested parameter group
-    #     :return: a dictionary of parameters
-    #     """
-    #     if groups is None:
-    #         groups = list(self.param_groups.keys())
-    #         if groups == ['default']:
-    #             return {name: param.value for name, param in self.param_groups['default'].items()}
-    #     elif not isinstance(groups, list):
-    #         groups = [groups]
-    #     return {group: {name: param.value for name, param in self.param_groups[group].items()} for group in groups}
-    #
-    # def get_value(self, name, group='default'):
-    #     return self.get_values(group)[group][name]
-    #
-    # def __repr__(self):
-    #     return f'{self.get_values()}'
-    #
-    # def to_dict(self):
-    #     groups = list(self.param_groups.keys())
-    #     if groups == ['default']:
-    #             return {name: param for name, param in self.param_groups['default'].items()}
-    #     return {group: {name: param for name, param in self.param_groups[group].items()} for group in groups}
