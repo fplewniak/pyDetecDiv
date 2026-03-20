5b36bbe28cb9cb94feee7b56df005483581626a5
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2026-03-20 17:17:50 +0100

    NDTiffImageResource now uses dask arrays for auto_channels, which should require less memory when cropping as the whole image does not need to be read from disk

--- src/pydetecdiv/domain/NDTiffImageResource.py
+++ src/pydetecdiv/domain/NDTiffImageResource.py
@@ -30,6 +30,7 @@ class NDTiffImageResource(ImageResourceData):
         self._dims = image_resource.dims
         self._drift = image_resource.drift
         self._ndtiff_ds = None
+        self._as_array = None
 
     @property
     def shape(self) -> tuple[int, int, int, int, int]:
@@ -90,6 +91,29 @@ class NDTiffImageResource(ImageResourceData):
             self._ndtiff_ds = NDTiffDataset(self.path[0])
         return self._ndtiff_ds
 
+    def channel_list(self, channel=None, z=None, time=None, sliceX=None, sliceY=None, alpha=False):
+        if not isinstance(channel, (tuple, list)):
+            channel = [channel]
+        img_list = []
+        for c in channel:
+            if c is not None:
+                img_list.append(Image(self.as_array[self.pos_in_array][z][time][c][sliceY, sliceX].compute().squeeze()))
+            else:
+                if sliceX.start and sliceY.start:
+                    img_list.append(Image(np.zeros((sliceY.stop - sliceY.start, sliceX.stop - sliceX.start), np.uint16)))
+                img_list.append(Image(np.zeros((self.sizeY, self.sizeX), np.uint16)))
+        return img_list
+
+    @property
+    def as_array(self):
+        if self._as_array is None:
+            self._as_array = self.ndtiff_ds.as_array(['position', 'channel', 'time', 'z'])
+        return self._as_array
+
+    @property
+    def pos_in_array(self):
+        return self.ndtiff_ds.axes['position'].index(self.pos_index)
+
     def _image(self, C: int = 0, Z: int = 0, T: int = 0, sliceX: slice = None, sliceY: slice = None,
                drift: bool = False) -> np.ndarray:
         """
@@ -131,6 +155,41 @@ class NDTiffImageResource(ImageResourceData):
         sliceY = slice(sliceY.start + deltaY, sliceY.stop + deltaY)
 
         return self._image(C, Z, T)[sliceY, sliceX]
+
+    def auto_channels(self, C: int = 0, T: int = 0, Z: int | list[int] | tuple[int] = 0,
+                      crop: tuple[slice, slice] = None, drift: bool = False, alpha: bool = False) -> Image:
+        """
+        Returns a RGB, RGBA or grayscale image depending upon the C or Z values. If C (or Z) is a tuple, it is used as
+        RGB values. If alpha is set to True, then the maximum value of every pixel across all channels defines its
+        alpha value. If C and Z are both an index, then the returned image is grayscale.
+
+        :param image_resource_data: the image resource data used to create the Image
+        :param C: the channel or channels tuple
+        :param T: the time frame index
+        :param Z: the z-slice or z-slices tuple
+        :param crop: a tuple defining the crop values as slices = (slice(xmin, xmax), slice(ymin, ymax))
+        :param drift: bool defining whether drift correction should be applied
+        :param alpha: bool defining whether the image should contain an alpha channel
+        :return: Image
+        """
+        img = None
+        sliceX = slice(None, None)
+        sliceY = slice(None, None)
+
+        if crop is not None:
+            deltaX = 0 if not drift or self.drift is None else int(round(self.drift.iloc[T].dx))
+            deltaY = 0 if not drift or self.drift is None else int(round(self.drift.iloc[T].dy))
+            sliceX = slice(crop[0].start + deltaX, crop[0].stop + deltaX)
+            sliceY = slice(crop[1].start + deltaY, crop[1].stop + deltaY)
+
+        # img_array = self.ndtiff_ds.as_array(['position', 'channel', 'time', 'z'])
+        if isinstance(C, int):
+            img = Image(self.as_array[self.pos_in_array][C][T][Z][sliceY, sliceX].compute())
+            # img = Image(self.ndtiff_ds.read_image(position=self.pos_index, channel=0, z=0, time=T)[sliceY, sliceX])
+        elif isinstance(C, (tuple, list)):
+            # img = Image.compose_channels([Image(self.as_array[self.pos_in_array][Z][T][c][sliceY, sliceX].compute().squeeze()) for c in C], alpha=alpha)
+            img = Image.compose_channels(self.channel_list(channel=C, z=Z, time=T, sliceX=sliceX, sliceY=sliceY), alpha=alpha)
+        return img
 
     # def data_sample(self, X: slice = None, Y: slice = None) -> np.ndarray:
     #     """
