6305262537b45228e5c74e262f353fbbcb613247
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2026-04-07 14:55:53 +0200

    Fixed correspondance between annotation index (1-based) and class names (0-based index)

--- src/pydetecdiv/domain/RoiAnnotations.py
+++ src/pydetecdiv/domain/RoiAnnotations.py
@@ -15,7 +15,7 @@ class RoiAnnotations(DomainSpecificObject):
         super().__init__(**kwargs)
         self._roi = roi.id_ if isinstance(roi, ROI) else roi
         self._classification = classification.id_ if isinstance(classification, Classification) else classification
-        self._annotation = self.classification.classes.index(annotation) if isinstance(annotation, str) else annotation
+        self._annotation = self.classification.classes.index(annotation) + 1 if isinstance(annotation, str) else annotation
         self.t = t
         self._run = run.id_ if isinstance(run, Run) else run
         self.key_val = key_val
@@ -35,7 +35,7 @@ class RoiAnnotations(DomainSpecificObject):
 
     @property
     def class_name(self):
-        return self.classification.classes[self._annotation]
+        return self.classification.classes[self._annotation - 1]
 
     @property
     def run(self):
