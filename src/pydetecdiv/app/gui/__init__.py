627a2c1b987c889e54e5262f68992538de72adea
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2026-04-27 17:35:12 +0200

    Removed ActionsDeepLearning import as this module does not exist any more

--- src/pydetecdiv/app/gui/__init__.py
+++ src/pydetecdiv/app/gui/__init__.py
@@ -12,7 +12,7 @@ from PySide6.QtGui import QAction, QIcon, QFont
 from PySide6.QtWidgets import QToolBar, QStatusBar, QMenu, QApplication, QDialog, QDialogButtonBox, QSizePolicy, QLabel
 
 from pydetecdiv.app import PyDetecDiv
-from pydetecdiv.app.gui import ActionsSettings, ActionsProject, ActionsData, ActionsDeepLearning
+from pydetecdiv.app.gui import ActionsSettings, ActionsProject, ActionsData
 import pydetecdiv.app.gui.resources_rc
 from pydetecdiv.app.gui.ActionsData import ImportROIannotations
 from pydetecdiv.app.gui.tools.data.hdf5 import Create_ROI_HDF5
