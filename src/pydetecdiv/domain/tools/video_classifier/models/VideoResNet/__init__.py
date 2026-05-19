504078c4b00c4528071da20abd745aafa815f896
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2026-05-19 14:43:43 +0200

    Added Xavier uniform weight initialization to Linear classification head in R2+1D video classification model

--- src/pydetecdiv/domain/tools/video_classifier/models/VideoResNet/__init__.py
+++ src/pydetecdiv/domain/tools/video_classifier/models/VideoResNet/__init__.py
@@ -11,8 +11,8 @@ class R3D_18(nn.Module):
         self.r3d_18 = r3d_18(weights=R3D_18_Weights.DEFAULT)
 
         self.r3d_18.fc = nn.Sequential(nn.Dropout(p=dropout, inplace=True),
-                                  nn.Linear(in_features=512, out_features=n_classes, bias=True)
-                                  )
+                                       nn.Linear(in_features=512, out_features=n_classes, bias=True)
+                                       )
 
     def forward(self, x):
         x = x.permute(0, 2, 1, 3, 4)
@@ -29,8 +29,8 @@ class MC3_18(nn.Module):
         self.mc3_18 = mc3_18(weights=MC3_18_Weights.DEFAULT)
 
         self.mc3_18.fc = nn.Sequential(nn.Dropout(p=dropout, inplace=True),
-                                  nn.Linear(in_features=512, out_features=n_classes, bias=True)
-                                  )
+                                       nn.Linear(in_features=512, out_features=n_classes, bias=True)
+                                       )
 
     def forward(self, x):
         x = x.permute(0, 2, 1, 3, 4)
@@ -44,11 +44,16 @@ class R2Plus1d_18(nn.Module):
 
         self.expected_shape = ('Batch', 4, 3, 224, 224)
 
-        self.r2plus1d_18 = r2plus1d_18(weights= R2Plus1D_18_Weights.DEFAULT)
+        self.r2plus1d_18 = r2plus1d_18(weights=R2Plus1D_18_Weights.DEFAULT)
+
+        self.linear = nn.Linear(in_features=512, out_features=n_classes, bias=True)
+        nn.init.xavier_uniform_(self.linear.weight)
+        if self.linear.bias is not None:
+            nn.init.zeros_(self.linear.bias)
 
         self.r2plus1d_18.fc = nn.Sequential(nn.Dropout(p=dropout, inplace=True),
-                                  nn.Linear(in_features=512, out_features=n_classes, bias=True)
-                                  )
+                                            self.linear
+                                            )
 
     def forward(self, x):
         x = x.permute(0, 2, 1, 3, 4)
