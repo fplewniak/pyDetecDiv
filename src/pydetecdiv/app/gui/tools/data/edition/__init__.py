6d9859b85cef39954d6cf9f7290c5700fa9ecd5f
Frédéric PLEWN IAK <f.plewniak@unistra.fr>
2026-08-14 15:22:00 +0200

    functions in Enable.py have been moded to utils/check.py for wider use than simply for enabling actions and menus in GUI

--- src/pydetecdiv/app/gui/tools/data/edition/__init__.py
+++ src/pydetecdiv/app/gui/tools/data/edition/__init__.py
@@ -3,7 +3,7 @@ Data edition tool classes for GUI
 """
 from typing import Any, Callable
 
-from pydetecdiv.app.gui import Enable
+from pydetecdiv.utils import check
 from pydetecdiv.app.gui.tools import ToolMenu, ToolAction
 from pydetecdiv.app.gui.tools.data.edition.drift_correction import ComputeDriftDialog
 
@@ -14,7 +14,7 @@ class DriftCorrectionMenu(ToolMenu):
     """
     def __init__(self, tool_name: str, enable: Callable[..., Any] | None = None, **kwargs: dict[str, Any]):
         super().__init__(tool_name, enable=enable, **kwargs)
-        ToolAction(tool_name, 'compute_drift', ComputeDriftDialog, self, enable=Enable.if_image_resources)
+        ToolAction(tool_name, 'compute_drift', ComputeDriftDialog, self, enable=check.if_image_resources)
         apply_drift = ToolAction(tool_name, 'apply_drift_correction', self.tool.apply_drift_correction, self,
-                                 enable=Enable.if_drift_correction)
+                                 enable=check.if_drift_correction)
         apply_drift.setCheckable(True)
--- src/pydetecdiv/app/gui/tools/deep_learning/classification_schemes/__init__.py
+++ src/pydetecdiv/app/gui/tools/deep_learning/classification_schemes/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from pydetecdiv.app.gui import Enable
+from pydetecdiv.utils import check
 from pydetecdiv.app.gui.tools import ToolMenu, ToolAction
 from pydetecdiv.app.gui.tools.deep_learning.classification_schemes.manage import ManageClassificationSchemeDialog
 
@@ -8,4 +8,4 @@ from pydetecdiv.app.gui.tools.deep_learning.classification_schemes.manage import
 class ClassificationSchemeMenu(ToolMenu):
     def __init__(self, tool_name: str, **kwargs: dict[str, Any]):
         super().__init__(tool_name, **kwargs)
-        ToolAction(tool_name, 'manage_schemes', ManageClassificationSchemeDialog, self, enable=Enable.if_project_exists)
+        ToolAction(tool_name, 'manage_schemes', ManageClassificationSchemeDialog, self, enable=check.if_project_exists)
